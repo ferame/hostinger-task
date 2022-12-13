656d7001a2a6168e14f2d87e9f71d9233dd9e216
ferame <alisauskas.j@gmail.com>
2022-12-13 23:57:16 +0200

    Made AutocompleteButton into a separate component, fixed loading animation

--- src/components/autocomplete-search/AutocompleteSearch.tsx
+++ src/components/autocomplete-search/AutocompleteSearch.tsx
@@ -1,17 +1,13 @@
 import { useState, useCallback, Fragment, createContext } from 'react';
 import { debounce } from '../../utils/debounce';
 import { SuggestionsList } from '../../components/suggestions-list/SuggestionsList';
-import magnifyingGlass from '../../assets/magnifying-glass.svg';
 import { suggestionsApiClient } from '../../api/suggestionsApiClient';
+import { AutocompleteButton } from '../autocomplete-button/AutocompleteButton';
 
 interface IAutocompleteSearch {
   suggestionsLimit: number;
   autocompleteDelayTimeMs: number;
 }
-
-const LOADING_BUTTON_STYLING =
-  'border-6 border-t-6 border-hostinger-hover-grey border-t-hostinger-purple-deep animate-spin';
-const SEARCH_BUTTON_STYLING = 'focus:border-2 focus:border-hostinger-purple-opaque';
 
 export const AutocompleteSearch: React.FC<IAutocompleteSearch> = ({ suggestionsLimit, autocompleteDelayTimeMs }) => {
   const [textInput, setTextInput] = useState('');
@@ -53,17 +49,7 @@ export const AutocompleteSearch: React.FC<IAutocompleteSearch> = ({ suggestionsL
           value={textInput}
           onChange={(event) => updateValue(event?.target?.value)}
         />
-        <button
-          data-testid="autocomplete-search-button"
-          className={`rounded-full outline-none w-12 h-12 bg-hostinger-purple text-white my-0.5 ml-0.5 mr-1 hover:bg-hostinger-purple-deep ${
-            isLoading ? LOADING_BUTTON_STYLING : SEARCH_BUTTON_STYLING
-          }`}
-          onClick={() => debouncedGetSuggestions(textInput)}
-        >
-          {isLoading ? null : ( //Issue with placement of svg when spinning.
-            <img src={magnifyingGlass} alt="search" height={15} width={15} className={'mx-auto'} />
-          )}
-        </button>
+        <AutocompleteButton isLoading={isLoading} onClick={() => debouncedGetSuggestions(textInput)} />
       </div>
       {isApiClientCalled || textInput.length > 0 ? (
         <SuggestionsList
