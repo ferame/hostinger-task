fd179b3c67195399d85a056b1a4d8b1ec832f308
ferame <alisauskas.j@gmail.com>
2022-12-13 16:03:49 +0200

    Moved list entry onClick handling to button.  WebsiteBuilder css improvements

--- src/pages/website-builder/WebsiteBuilder.tsx
+++ src/pages/website-builder/WebsiteBuilder.tsx
@@ -3,15 +3,14 @@ import { AutocompleteSearch } from '../../components/autocomplete-search/Autocom
 const SUGGESTIONS_LIMIT = 10;
 const DEBOUNCE_TIME_MS = 1000;
 
-// TODO: this component can be made into reusable autocomplete component and into separate WebsiteBuilder component
 export const WebsiteBuilder = () => {
   return (
     <div data-testid="website-builder" className="pt-10 flex flex-col items-center">
-      <h1 className="pb-3 text-3.5xl">What type of website are you building?</h1>
-      <p className="pb-10 text-base text-hostinger-text-grey font-light">
+      <h1 className="pb-3 text-3.5xl px-2 text-center">What type of website are you building?</h1>
+      <p className="pb-10 px-2 text-base text-hostinger-text-grey font-light text-center">
         Start typing and get the suggestions to choose from.
       </p>
-      <div className="w-96">
+      <div className="min-w-fit w-full max-w-sm px-2">
         <AutocompleteSearch suggestionsLimit={SUGGESTIONS_LIMIT} autocompleteDelayTimeMs={DEBOUNCE_TIME_MS} />
       </div>
     </div>
